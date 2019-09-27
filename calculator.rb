3c5a8111b154d85313e6d7759619768e8e5483a6
Randy Steele <steelerx155@gmail.com>
2019-09-27 00:42:29 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,3 @@ sum = get_variable_from_file('./calculator.rb', "sum")
 difference = get_variable_from_file('./calculator.rb', "difference")
 product = get_variable_from_file('./calculator.rb', "product")
 quotient = get_variable_from_file('./calculator.rb', "quotient")
-end
