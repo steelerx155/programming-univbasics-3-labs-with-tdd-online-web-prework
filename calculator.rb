0cf9d3a04b4f5a2362794f1b289b3667001f1cb0
Randy Steele <steelerx155@gmail.com>
2019-09-27 00:38:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+first_number = get_variable_from_file('./calculator.rb', "first_number")
+second_number = get_variable_from_file('./calculator.rb', "second_number")
+sum = get_variable_from_file('./calculator.rb', "sum")
+difference = get_variable_from_file('./calculator.rb', "difference")
+product = get_variable_from_file('./calculator.rb', "product")
+quotient = get_variable_from_file('./calculator.rb', "quotient")
+end
